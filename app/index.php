a33e0e5a8f4a7b9df0029f7bd556e84ca67d920f
sergi-tsel <tselishchevsy@gmail.com>
2023-02-08 20:36:03 +0300

    add index, composer and bootstrap

---
+++ app/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/docs-app/css/dist/mdb5/standard/core.min.css">
+    <title>Блог</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-black">
+    <div class="container-fluid row">
+        <a class="navbar-brand col-1" href="/">Блог</a>
+        <div class="navbar-collapse col-11" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link" href="/profile/administer">Вход</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<?php
+require '../vendor/autoload.php';
+require_once 'Bootstrap.php';
+?>
+</body>
+</html>
